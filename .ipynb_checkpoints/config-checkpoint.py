9c392d157a8315f5a409e4208fb724ece31135cb
Marcio <92833805+mciano@users.noreply.github.com>
2021-12-19 17:37:26 -0500

    add challenge files

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b1e69030",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "db_password = 'C14n0Guilherme*@!'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "PythonData",
+   "language": "python",
+   "name": "pythondata"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
